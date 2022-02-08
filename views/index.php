3fc36ceea7f9b74c2f2e4b74d97582c76523711d
owenclamor <owenclamor22@gmail.com>
2022-02-08 15:41:03 +0800

    modified landing page to bootstrap

--- views/index.php
+++ views/index.php
@@ -1,151 +1,191 @@
 <?php require_once 'include/headers.php'?>
 
-    <!-- <div class="container p-3 mt-3 col-4 shadow">
-        <div class="text-center">
-            <h1 class="fw-lighter">./strap-landing-page</h1>
 
-            <a href="/login">login</a>
-            <a href="/register">register</a>
-            <a href="/dashboard">dashboard</a>
-            <a href="/landing">test link</a>
+<section class="jumbotron bg-image">
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0);"></div>
+        <nav class="navbar navbar-expand-xl navbar-light">
+            <div class="navbar-container container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="../assets/img/strap logo.png" alt="">  STRAP</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo2" aria-controls="navbarTogglerDemo2" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse show" id="navbarTogglerDemo2">
+                <ul class="navbar-nav  me-auto mb-2 mb-xl-0">
+                  <li class="nav-item">
+                    <a class="nav-item-link nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-item-link nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-item-link nav-link" href="#">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-item-link nav-link" href="#">Contact Us</a>
+                  </li>
+                </ul>
+    
+                <form class="d-flex">
+                    <button class="btn btn-light me-2" type="button"><a class="login-button" href="/login">Log In</a></button>
+                    <button class="btn btn-sm btn-primary" type="button">Sign Up</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+
+        <div class="mt-4 p-5 rounded jumbotron-container">
+            <p class="what-we-do">What We Do</p>
+            <h1 class="jumbotron-header">A Platform Build For Pets</h1>
+        </div>
+    </section>
+
+
+    <section class=" row about-us-section">
+        <div class="col mt-4 p-5 rounded jumbotron-container aboutus-container">
+            <p class="aboutus">About Us</p>
+            <h1 class="aboutus-header">Specialized TRAcker for Pets (STRAP)</h1>
+            <p class="aboutus-desc"> It aims to provide real-time data location to the pets of the owner by using a Global Positioning System (GPS) collars.</p>
+     
+            <p class="aboutus-desc">Also, it is a platform for pet owners to store their pet’s identification information.</p>
+            <br>
             
-        </div> 
-    </div> -->
-
-    <nav class="navbar-container">
-        <div class="logo-container"> 
-            <img class="logo" src= "../img/strap logo.png" alt="">
-            <span class="title">
-                STRAP
-            </span>
+            <button class="about-button btn btn-sm btn-outline-light" type="button">Learn More</button>
         </div>
-          
-        <ul class="nav-container">
-            <div class="nav-navbar">
-                <li class="nav-item">Home</li>
-                <li class="nav-item">About</li>
-                <li class="nav-item">Services</li>
-                <li class="nav-item">Contact Us</li>   
-            </div>    
-        </ul>
-    
-            <div class="nav-user">
-                          
-                    <a href="/login" class="btn login" href=""> Log In</a>
-                    
-                    <a href="/register" class="btn signup"href=""> Sign-Up</a>              
-            </div>
-    </nav>
-
-    
-    <section class="hero-section">
-        <div class="header-container">
-            <h3 class="header intro-header">WHAT WE DO?</h3>
-            <h1 class="header desc-header">A platform built for pets</h1>
+        <div class="col">
+            <img class="about-us-img"  src="../assets/img/yellow-lab.png" alt="">
         </div>
     </section>
 
-    <div class="bg-about"></div>
-    <section class="about-us">
+    <section class="features-section">
+        <h1 class="feature-header">STRAP'S OBJECTIVES</h1>
+        <div class="box-container">
+            <div class="box">
+                <img class="figure" src="../assets/icons/gps-img.png" alt="">
+                <h5 class="box-header">GPS Collar</h5>
+                <p>To build a GPS collar using Arduino for real-time data location of the pets.
+                </p>
+            </div>
+            <div class="box">
+                <img class="figure"  src="../assets/icons/web-img.png" alt="">
+                <h5 class="box-header">Web Application</h5>
+                <p>To create a web application that stores the pet’s identification information. </p>
+            </div>
+            <div class="box">
+                <img class="figure" src="../assets/icons/qr-img.png" alt="">
+                <h5 class="box-header">Quick Response Code</h5>
+                <p>To allow users to access the pet’s personal information using a Quick Response (QR) code from the specialized collar.
+                </p>
+            </div>
+        </div>
+        <div class="button-feature">
+            <button class="btn btn-sm btn-outline-light" type="button">Learn More</button>
+        </div>
         
-        <div>
-            <h5>ABOUT US</h5>
-            <h1 class="about-name">Specialized TRAcker for Pets (STRAP)</h1>
-            <p class="about-desc">It aims to provide real-time data location to the pets of the owner by using
-                a Global Positioning System (GPS) collars.</p>
-    
-            <p class="about-desc p2">Also, it's a platform for pet owners to store their pet's identification information.</p>
-    
-            <a class="about-btn" href="">LEARN MORE</a>
-        </div>
-        <div class="about-img-container">
-            <img class="about-img" src="assets/img/yellow-lab.png" alt="">
-            <p class="about-img-desc">YELLOW LABRADOR WITH TONGUE OUT BY LUCAS LUDWIG</p>
-        </div>
-       
     </section>
 
-    <section class="objectives-section">
-        <h1 class="obj-header">STRAP'S OBJECTIVES</h1>
 
-        <div class="card-obj-container">
-            <div class="card">
-                <div>
-                    <figure>
-                        <img src="assets/icons/gps-img.png" alt="">
-                    </figure>
-                    <p class="card-desc">GPS Collar</p>
-                    <div class="card-info-container">
-                        <p class="card-info"> To build a GPS collar using Arduino for real-time data location of the pets.</p>
-                    </div>
-                    
-                </div>
-                
-            </div>
-            <div class="card">
-                <div>
-                    <figure>
-                        <img src="assets/icons/web-img.png" alt="">
-                    </figure>
-                    <p class="card-desc">Web Application</p>
-                    <div class="card-info-container">
-                        <p class="card-info">To create a web application that stores the pet’s identification information. </p>
-                    </div>
-                    
-                </div>
-                
-            </div>
-            <div class="card">
-                <div>
-                    <figure>
-                        <img src="assets/icons/qr-img.png" alt="">
-                    </figure>
-                    <p class="card-desc">Quick Response (QR) code</p>
-                    <div class="card-info-container">
-                        <p class="card-info">To allow users to access the pet’s personal information using a Quick Response (QR) code from the specialized collar.
-                    </div>
-                    
-                    </p>
-                </div>
-                
-            </div>
 
+
+
+
+
+    <!-- Footer -->
+<footer class="footer-section text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+    <section
+      class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+    >
+  
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            
+            <h6 class="text-uppercase fw-bold mb-4">
+                <img src="img/strap logo.png" alt="">
+              <i class="fas fa-gem me-3"></i>STRAP
+            </h6>
+            <p>
+                aims to provide real-time data location to the pets of the owner by using a Global Positioning System (GPS) collars. 
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+                Menu
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Home</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">About</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Services</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Contact</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+                Contact
+            </h6>
+            
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              STRAP@examplegmail.com
+            </p>
+            
+            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          </div>
+          <!-- Grid column -->
         </div>
-
+        <!-- Grid row -->
+      </div>
     </section>
-
-    
-    <section class="footer-section">
-        <footer>
-            <div class="header-div">
-                <div class="test">
-                    <figure>
-                        <!-- <img src="assets/img/strap logo.png" alt=""> -->
-                    </figure>
-                    <h2>STRAP</h2>
-                </div>
-                
-                <p>MENU</p>
-                <p>CONTACT US</p>
-                <p>FOLLOW US</p>
-            </div>
-            <div class="footer-items">
-                <p class="strap-desc">Aims to provide real-time data location to the pets of the owner by using a Global Positioning System (GPS) collars. </p>
-                <div>
-                    <p>Home</p>
-                    <p>About</p>
-                    <p>Services</p>
-                    <p>Contact</p>
-                </div>
-                <div>
-                    <p>Email: strap@gmail.com</p>
-                    <p>Phone: 856 - 693 - 456</p>
-                </div>
-
-                <div>
-                    <p>LOGOS HERE</p>
-                </div>
-            </div>
-        </footer>
-    </section>
+    <!-- Section: Links  -->
+  
+  </footer>
+  <!-- Footer -->
+   
 <?php require_once 'include/footers.php'?>
