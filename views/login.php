053019aca5a4a2a1532b37995d99fbfbdd956cf7
Ablaze0313 <61894984+Ablaze0313@users.noreply.github.com>
2022-02-22 20:24:25 +0900

    NEW LOGIN AND REGISTER PAGE

--- views/login.php
+++ views/login.php
@@ -1,23 +1,147 @@
 <?php require_once 'include/headers.php'?>
-<div class="container col-5 mt-3 p-4 shadow">
-<form action="/process-login.php" novalidate="" method="POST">
-  <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" name="email">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" name="password">
-    </div>
-  </div>
-  
-  
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
-</div>
+<body>
+    <main>
+      <div class="box">
+        <div class="inner-box">
+          <div class="forms-wrap">
+            <form action="/login" autocomplete="off" class="sign-in-form">
+              <div class="logo">
+                <img src="assets/img/strap-logo.png" alt="strap" />
+                <h4>STRAP</h4>
+              </div>
+
+              <div class="heading">
+                <h2>Welcome Back</h2>
+                <h6>Not registred yet?</h6>
+                <a href="#" class="toggle">Sign up</a>
+              </div>
+
+              <div class="actual-form">
+                <div class="input-wrap">
+                  <input
+                    type="text"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Username</label>
+                </div>
+
+                <div class="input-wrap">
+                  <input
+                    type="password"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Password</label>
+                </div>
+
+                <input type="submit" value="Sign In" class="sign-btn" />
+
+                <p class="text">
+                  Forgotten your password or you login details?
+                  <a href="#">Get help</a> signing in
+                </p>
+              </div>
+            </form>
+
+            <form action="/login" autocomplete="off" class="sign-up-form">
+              <div class="logo">
+                <img src="assets/img/strap-logo.png" alt="STRAP" />
+                <h4>STRAP</h4>
+              </div>
+
+              <div class="heading">
+                <h2>Get Started</h2>
+                <h6>Already have an account?</h6>
+                <a href="#" class="toggle">Sign in</a>
+              </div>
+
+              <div class="actual-form">
+                <div class="input-wrap">
+                  <input
+                    type="text"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Username</label>
+                </div>
+
+                <div class="input-wrap">
+                    <input
+                      type="text"
+                      minlength="4"
+                      class="input-field"
+                      autocomplete="off"
+                      required
+                    />
+                    <label>Email</label>
+                  </div>
+
+                <div class="input-wrap">
+                  <input
+                    type="email"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Address</label>
+                </div>
+
+                <div class="input-wrap">
+                  <input
+                    type="password"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Password</label>
+                </div>
+
+                <input type="submit" value="Create my Account" class="sign-btn" />
+
+                <p class="text">
+                  By signing up, I agree to the
+                  <a href="#">Terms of Services</a> and
+                  <a href="#">Privacy Policy</a>
+                </p>
+              </div>
+            </form>
+          </div>
+
+          <div class="carousel">
+            <div class="images-wrapper">
+              <img src="assets/img/pet.png" class="image img-1 show" alt="" />
+              <img src="assets/img/pet2.jpg" class="image img-2" alt="" />
+              <img src="assets/img/pet3.png" class="image img-3" alt="" />
+            </div>
+
+            <div class="text-slider">
+              <div class="text-wrap">
+                <div class="text-group">
+                  <h2>Real-time data location of the pets</h2>
+                  <h2>Stores pet's information</h2>
+                  <h2>Access pet's information using QR</h2>
+                </div>
+              </div>
+
+              <div class="bullets">
+                <span class="active" data-value="1"></span>
+                <span data-value="2"></span>
+                <span data-value="3"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
 <?php require_once 'include/footers.php'?>  
 
 
